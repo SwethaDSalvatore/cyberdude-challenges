568d2e681c900578ebf0affef1d28adfef55c733
SwethaDSalvatore <swetharamesh35309@gmail.com>
2024-01-10 19:40:20 +0530

    api logic completed

---
+++ javascript/javascript-DOM/06 - github_repo_lister/README.md
@@ -0,0 +1,59 @@
+# GitHub Repository Lister
+
+## Table of Contents
+
+- [Project Title](#project-title)
+- [Description](#description)
+- [Demo](#demo)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [How to Use](#how-to-use)
+- [API Key](#api-key)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Project Title
+
+GitHub Repository Lister
+
+## Description
+
+GitHub Repository Lister is a web application that allows users to list repositories of a GitHub user by providing the username. The project utilizes the GitHub Dev Resource API to fetch user repositories and display them in a user-friendly interface. The project is built using HTML, styled with Tailwind CSS, and enhanced with JavaScript for dynamic functionality.
+
+## Demo
+
+[Live Demo](https://swethadsalvatore.github.io/cyberdude-challenges/javascript/javascript-DOM/06%20-%20github_repo_lister/dist/)
+
+## Features Added
+
+- Display repositories of a GitHub user by inputting the username
+- Show repository details such as name and link
+- Responsive design for various screen sizes
+- User-friendly interface
+
+## Technologies Used
+
+- HTML
+- TailwindCSS
+- JavaScript
+
+## How to Use
+
+1. Clone the repository to your local machine.
+2. Obtain a GitHub Dev Resource API token (see [API Key](https://api.github.com/users/${inputUserName}) section for instructions).
+3. Open the `index.html` file in your web browser.
+4. Enter a GitHub username into the provided input field.
+5. Click the "check" button.
+6. View the list of repositories and their details.
+
+## License
+
+[MIT](./LICENSE.md)
+
+## Acknowledgments
+
+I want to express my gratitude to [Mr. Anbuselvan Rocky](https://github.com/anburocky3) for the invaluable guidance and support throughout the development of this project. Their insights and feedback have significantly contributed to the success of this endeavor. Special thanks to all my Colleague for their collaborative efforts and constructive suggestions.
+
+As I continue to advance in my programming journey, the skills acquired and lessons learned from this project will serve as a solid foundation for future endeavors.
+
+---
--- javascript/javascript-DOM/06 - github_repo_lister/assets/js/main.js
+++ javascript/javascript-DOM/06 - github_repo_lister/assets/js/main.js
@@ -1,14 +1,13 @@
 const formEl = document.forms["form"];
 const userNameEl = document.querySelector("#userName");
-const apiUrl = "https://api.github.com/users/<USER-NAME>/repos";
-
+const profile = document.getElementById("profile");
+profile.style.display = "none";
 
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
-
     // validation
     function userName() {
-        if (inputUserName.length > 5) {
+        if (inputUserName.length > 3) {
             return inputUserName;
         } else {
             alert("Invalid Username");
@@ -16,11 +15,39 @@ formEl.addEventListener("submit", (e) => {
     }
     // calling input userName
     const inputUserName = userNameEl.value;
-    const gitUserName = userName();
 
-    // repalce the api username with input name
-    const apiUrl = "https://api.github.com/users/<USER-NAME>/repos";
-    const gitname = apiUrl.replace("<USER-NAME>", gitUserName);
+    // create new XMLHttpRequest
+    const newXml = new XMLHttpRequest();
+    // console.log(newXml);
 
-    console.log(gitname);
+    newXml.open("GET", `https://api.github.com/users/${inputUserName}`);
+
+    newXml.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const data = JSON.parse(newXml.response);
+
+            const profileView = `
+            <div class="mt-5 flex space-x-10 items-center text-fuchsia-200 text-sm sm:text-xl bg-fuchsia-900 p-4 rounded border border-fuchsia-300">
+            <img src="${data.avatar_url}" alt="profile avatar"
+            class="w-32 h-32 rounded-full border border-fuchsia-100">
+          <div class="space-y-2" id="confirm">
+            <h4>👋Hi I am <span class="font-semibold">${data.name}</span></h4>
+            <h4>Are you Looking for My Github Repository List ?</h4>
+            <button type="submit"
+              class="px-6 py-1 rounded bg-fuchsia-950 hover:bg-fuchsia-900 text-fuchsia-100 text-sm sm:text-lg border border-fuchsia-300">
+              <a href="${data.html_url}" target="blank">Yes</a></button>
+          </div>
+          </div>
+            `
+            profile.style.display = "block";
+            profile.innerHTML = profileView;
+
+        }
+    };
+
+    newXml.send();
+
+
+
+
 });
